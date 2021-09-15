32f96679fcbdc2967337fc4d4b3e9cb31ad7441f
Adriana Benitez <83874053+a-benitez@users.noreply.github.com>
2021-09-15 15:32:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Algoritmo para calcular el valor total de un precio "x" + IVA (21%)
+
+//Datos de entrada
+let precioProducto = parseFloat(prompt("Ingrese el precio del producto "));
+let precioTotal = precioProducto * 1.21;
+
+//Datos de salida
+console.log("precio con IVA incluido: $" + precioTotal);
